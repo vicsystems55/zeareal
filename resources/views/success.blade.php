5e96e1af21f3e016efacbe13b6e08fbf63755d18
vicSystems <victorasuquob@gmail.com>
2023-05-05 17:54:50 +0100

    payment

--- resources/views/success.blade.php
+++ resources/views/success.blade.php
@@ -8,21 +8,8 @@
 
         <!-- start causes-single-wrapper -->
         <section class="causes-single-wrapper section-padding" style="padding-top: 12px;">
-            <div class="container">
-                <div class="row">
-                    <div class="col col-md-9">
-                        <div class="causes-single">
-
-                            <ul class="nav nav-pills">
-                                <li role="presentation" class="active"><a href="#">Home</a></li>
-                                <li role="presentation"><a href="#">Profile</a></li>
-                                <li role="presentation"><a href="#">Messages</a></li>
-                              </ul>
-                        </div> <!-- end causes single -->
-                    </div> <!-- end col -->
-
-
-                </div> <!-- end row -->
+            <div class="container d-flex align-items-center" style="padding-top: 120px; height: 300px;">
+                <h2 class="text-center ">Your donation has been received</h2>
 
 
             </div> <!-- end contianer -->
